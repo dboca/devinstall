c391de2b34b81f56cc7de571bd92b0d5f13a30a0
DBOCA <dragos.boca@gmail.com>
2013-02-26 01:11:09 +0200

    * Multiple projects

--- devinstall.gemspec
+++ devinstall.gemspec
@@ -4,20 +4,20 @@ $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require 'devinstall/version'
 
 Gem::Specification.new do |spec|
-  spec.name          = "devinstall"
+  spec.name          = 'devinstall'
   spec.version       = Devinstall::VERSION
-  spec.authors       = ["TODO: Write your name"]
-  spec.email         = ["TODO: Write your email address"]
-  spec.description   = %q{TODO: Write a gem description}
-  spec.summary       = %q{TODO: Write a gem summary}
-  spec.homepage      = ""
-  spec.license       = "MIT"
+  spec.authors       = ['Dragos Boca']
+  spec.email         = ['dboca@mail.com']
+  spec.description   = %q{remote builder and installer}
+  spec.summary       = %q{Copy the source files to a build host, build the packages and install builded packages}
+  spec.homepage      = ''
+  spec.license       = 'MIT'
 
   spec.files         = `git ls-files`.split($/)
   spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
-  spec.require_paths = ["lib"]
+  spec.require_paths = %w(lib)
 
-  spec.add_development_dependency "bundler", "~> 1.3"
-  spec.add_development_dependency "rake"
+  spec.add_development_dependency 'bundler'
+  spec.add_development_dependency 'rake'
 end
--- lib/devinstall.rb
+++ lib/devinstall.rb
@@ -1,5 +1,42 @@
-require "devinstall/version"
+require 'devinstall/version'
+require 'devinstall/settings'
 
 module Devinstall
-  # Your code goes here...
+  class Cli
+    extend self
+    @_package_version=''
+    @package_files   =Hash.new('')
+
+    def new (config_file)
+      Settings.load!(config_file)
+      package          =Settings.base[:package]
+      changelog        ="#{Settings.local[:folder]}/#{package}/debian/changelog"
+      @_package_version=File.open(changelog, 'r').gets.chomp.sub(/^.*\((.*)\).*$/, "\1")
+      pname            ="#{package}_#@_package_version"
+      @package_files   ={deb: "#{pname}_all.deb",
+                         tgz: "#{pname}.tar.gz",
+                         dsc: "#{pname}.dsc",
+                         chg: "#{pname}_debian.changes"}
+    end
+
+    def copy_to_build_host
+      rsync       =Settings.base[:rsync]
+      local_folder=Settings.local[:folder]
+      build_user  =Settings.build[:user]
+      build_host  =Settings.build[:host]
+      build_folder=Settings.build[:folder]
+
+      system("#{rsync} -az #{local_folder}/ #{build_user}@#{build_host}:#{build_folder}")
+    end
+
+    def build_package
+      package     =Settings.base[:package]
+      ssh         =Settings.base[:ssh_command]
+      build_user  =Settings.build[:user]
+      build_host  =Settings.build[:host]
+      build_folder=Settings.build[:folder]
+
+      system("#{ssh} #{build_user}@#{build_host} -c \" cd #{build_folder}/#{package} && dpkg-buildpackage\"")
+    end
+  end
 end
