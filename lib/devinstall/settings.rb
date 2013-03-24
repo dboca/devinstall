a936f406b51b911c9c8fec5d8a18b8e6e9bfee8e
DBOCA <dragos.boca@gmail.com>
2013-03-24 11:57:53 +0200

    commandline options check filename check

--- lib/devinstall/settings.rb
+++ lib/devinstall/settings.rb
@@ -2,20 +2,20 @@ require 'yaml'
 require 'devinstall/deep_symbolize'
 
 module Settings
-  # again - it's a singleton, thus implemented as a self-extended module
   extend self
 
   @_settings = {}
   attr_reader :_settings
 
   def load!(filename, options = {})
+    unless File.exist?(filename)
+      puts "Unable to find config file \"#{filename}\""
+      exit!(1)
+    end
     newsets = YAML::load_file(filename).deep_symbolize
     newsets = newsets[options[:env].to_sym] if options[:env] && newsets[options[:env].to_sym]
     deep_merge!(@_settings, newsets)
   end
-
-  # Deep merging of hashes
-  # deep_merge by Stefan Rusterholz, see http://www.ruby-forum.com/topic/142809
   def deep_merge!(target, data)
     merger = proc { |_, v1, v2|
       Hash === v1 && Hash === v2 ? v1.merge(v2, &merger) : v2 }
