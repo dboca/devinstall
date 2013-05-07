db2a257f65d2e9b6aef24d428377a14270e5f321
Dragos Boca <dboca@mail.com>
2013-05-07 17:51:23 +0300

    provider for local actions

---
+++ lib/devinstall/provider/provider_local.rb
@@ -0,0 +1,38 @@
+require 'devinstall/utils'
+require 'devinstall/settings'
+
+module Provider
+  module Local
+    include Utils
+
+    SETTINGS = {
+      local: [:copy, :sudo],
+    }
+
+    def upload_sources(cfg, src, dst)
+      config =Devinstall::Settings.instance
+      copy_command=config.local[:copy]
+      command("#{copy_command} #{src} #{dst}") unless src == dst
+    end
+
+    def download_file(cfg, file, local)
+      config=Devinstall::Settings.instance
+      copy_command=config.local[:copy]
+      command("#{copy_command} #{cfg[:target]}/#{file.to_s} #{local}")
+    end
+
+    def upload_file(cfg, file, local)
+      config=Devinstall::Settings.instance
+      copy_command=config.local[:copy]
+      command("#{copy_command} #{local}/#{file} #{cfg[:folder]}")
+    end
+
+    def exec_command(cfg, command)
+      config=Devinstall::Settings.instance
+      sudo=config.local[:sudo]
+      command("#{sudo} #{command}")
+    end
+
+  end #Local
+end #Provider
+
